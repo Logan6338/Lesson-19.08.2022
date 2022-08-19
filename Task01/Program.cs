579989431fb2ca676285a8c7d06e1d34ac03f801
Dmitry <Logan6338@mail.ru>
2022-08-19 20:44:04 +0300

    Задача 1

---
+++ Task01/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.*/
+
+int[] array = new int[12];
+
+int[] FillArray(int[] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = random.Next(-9,10);
+    }
+    return arr;
+}
+ void PrintArray(int[] arr)
+{
+   for (int i = 0; i < arr.Length; i++)
+{
+      System.Console.Write($"{arr[i]} ");
+ }
+}
+
+int FindSumPositiveNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] > 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int FindSumNegativeNumbers(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < 0)
+        {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+int[] Fill = FillArray(array);
+PrintArray(Fill);
+int sumPositive = FindSumPositiveNumbers(Fill);
+int sumnegative = FindSumNegativeNumbers(Fill);
+System.Console.WriteLine(sumPositive);
+System.Console.WriteLine();
+System.Console.WriteLine(sumnegative);
