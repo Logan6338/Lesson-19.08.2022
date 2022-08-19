1ee036b9a47bc9a7c6d89c1039e574aa6e38de71
Dmitry <Logan6338@mail.ru>
2022-08-19 21:17:43 +0300

    Задача 2

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = {-4, -8, 8, 2};
+
+
+int[] Reverse(int[] array)
+{
+
+int[] NewArray = new int[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+    NewArray[i] = array[i] * -1;
+
+}
+return NewArray;
+
+}
+
+int[] result = Reverse(array);
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
+
+
