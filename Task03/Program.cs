3e9b3371b99a4374ca85b3c0e2b33c4c1ea50fe8
Dmitry <Logan6338@mail.ru>
2022-08-19 22:03:23 +0300

    Задача 33

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[] { 6, 7, 19, 345, 3 }; 
+ 
+bool FindNumber(int N, int[] array) 
+{ 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        if (array[i] == N) 
+        { 
+            return true; 
+        } 
+    } 
+    return false; 
+} 
+int M = Convert.ToInt32(Console.ReadLine()); 
+bool res = FindNumber(M, arr); 
+Console.WriteLine(res);
